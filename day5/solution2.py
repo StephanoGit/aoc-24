b5e54c021b9ad39131ae52e0d55ced436e18d502
stephano <poporaduu@gmail.com>
2025-12-13 20:35:13 +0000

    add: solution2 for day5

--- day5/solution2.py
+++ day5/solution2.py
@@ -11,21 +11,27 @@ def print_queue(input):
     
 
     def check_update(update):
-        for f, s in rules:
+        modified = False
+        i = 0
+        while i < len(rules):
+            f, s = rules[i]
             if f in update and s in update:
                 f_index_in_u = update.index(f)
                 s_index_in_u = update.index(s)
                 if f_index_in_u > s_index_in_u:
-                    return False
-        return True
-
+                    update[f_index_in_u] = s
+                    update[s_index_in_u] = f
+                    modified = True 
+                    i = 0 
+            i += 1
+        return modified
 
     valid_updates = []
 
     for u in updates:
         if check_update(u):
             valid_updates.append(u) 
-   
+  
     return sum(u[len(u)//2] for u in valid_updates)
 
      
